58d005beb301ed9e93814203f7e3dce6056e6a72
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-07-01 15:14:13 -0700

    1606

---
+++ 1606/b.py
@@ -0,0 +1,18 @@
+import collections as cl, sys
+
+def main():
+    f = open(sys.argv[1] if len(sys.argv) > 1 else 'in')
+    lines = [l.rstrip('\n') for l in f]
+
+    # counts[pos][ch] = how many times character `ch` appeared in position `pos`
+    counts = cl.defaultdict(cl.Counter)
+    for line in lines:
+        for i, c in enumerate(line):
+            counts[i][c] += 1
+
+    for pos in sorted(counts):
+        print(min(counts[pos],
+                  key=lambda ch: counts[pos][ch]),
+              end='')
+
+main()
