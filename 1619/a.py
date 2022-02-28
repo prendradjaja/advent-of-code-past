5ce533d2c4a01463bac673a32cd335df4bb30941
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-02-27 21:12:44 -0600

    1619a: Don't need to use elf objects

--- 1619/a.py
+++ 1619/a.py
@@ -8,31 +8,22 @@ n = 5
 n = 3012210
 
 
-Elf = collections.namedtuple('Elf', 'id presents')
-
-
 def main():
     starttime = datetime.now()
-    print('Initializing...')
     elves = collections.deque(
-        Elf(i + 1, 1)
-        for i in range(n)
+        range(1, n+1)
     )
-    print(f'  Done after {human_readable(datetime.now() - starttime)}.')
-    print('\nSimulating...')
     progress = ProgressBar(n)
     while len(elves) > 1:
         first = elves.popleft()
-        second = elves.popleft()
-        elves.append(
-            Elf(first.id, first.presents + second.presents)
-        )
+        _ = elves.popleft()  # Discard the second elf
+        elves.append(first)
         if len(elves) % 1123 == 0:
             progress.show(n - len(elves))
     progress.done()
 
-    print('\nAnswer:')
-    print(elves[0].id)
+    print('Answer:')
+    print(elves[0])
 
 
 
