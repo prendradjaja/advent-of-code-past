63db0978b4f1ee19403a68c9e6e89632ed4f1dd8
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-08-07 14:49:26 -0500

    1717a: Clean up a bit

--- 1717/a.py
+++ 1717/a.py
@@ -1,9 +1,5 @@
-import sys
-
-
-STEP_COUNT = 301
-
-INSERTION_COUNT = 2017
+STEPS = 301
+INSERTIONS = 2017
 
 
 class CircularBuffer:
@@ -11,8 +7,8 @@ class CircularBuffer:
         self.items = [0]
         self.current_position = 0
 
-    def advance(self):
-        self.current_position = (self.current_position + STEP_COUNT) % len(self.items)
+    def advance(self, steps):
+        self.current_position = (self.current_position + steps) % len(self.items)
 
     def insert(self, value):
         self.items[self.current_position + 1 : ] = (
@@ -21,23 +17,14 @@ class CircularBuffer:
         )
         self.current_position += 1
 
-    def __str__(self):
-        result = ''
-        for i, item in enumerate(self.items):
-            if i != self.current_position:
-                result += f'{item} '
-            else:
-                result += f'({item}) '
-        return result
-
     def get(self, index):
         return self.items[index % len(self.items)]
 
 
 def main():
     buf = CircularBuffer()
-    for n in range(1, INSERTION_COUNT+1):
-        buf.advance()
+    for n in range(1, INSERTIONS + 1):
+        buf.advance(STEPS)
         buf.insert(n)
     print(buf.get(buf.current_position + 1))
 
